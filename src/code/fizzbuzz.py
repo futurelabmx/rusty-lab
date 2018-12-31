02c2f1b7f5c56e5afba542a89ae50a28ca500d05
VentGrey <omarpurataf@gmail.com>
2018-12-31 17:56:31 -0600

    Added fizzbuzz in python

---
+++ src/code/fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(x):
+    if x % 3 == 0 and x % 5 == 0:
+        return "FizzBuzz"
+    elif x % 3 == 0:
+        return "Fizz"
+    elif x % 5 == 0:
+        return "Buzz"
+    else:
+        return str(x)
+
+print("\n".join(fizzbuzz(x) for x in range(1, 100)))
